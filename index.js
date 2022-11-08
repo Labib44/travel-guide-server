9bafcf9677f875249792a6ae17c389e761d4f965
Labib44 <mdlabib162@gmail.com>
2022-11-09 02:10:41 +0600

    Create Services Api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app=express();
@@ -23,8 +23,21 @@ async function run(){
         app.get('/services', async(req, res)=>{
             const query={}
             const cursor=serviceCollection.find(query);
+            const services=await cursor.limit(3).toArray();
+            res.send(services);
+        })
+        app.get('/allservices', async(req, res)=>{
+            const query={}
+            const cursor=serviceCollection.find(query);
             const services=await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/services/:id', async(req, res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const details=await serviceCollection.findOne(query);
+            res.send(details);
         })
     }
     finally{
