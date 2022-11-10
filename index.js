e61ff1b0d4e1adc9eb0992b05fed70eb1ece1dfd
Labib44 <mdlabib162@gmail.com>
2022-11-10 23:27:40 +0600

    final edit done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express=require('express');
-const cors =require('cors');
-const { MongoClient, ServerApiVersion,  } = require('mongodb');
-const ObjectId=require('mongodb').ObjectId
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 
 require('dotenv').config();
 
-const app=express();
-const port= process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 // meddle wares
@@ -18,74 +19,103 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r6wv8dh.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
-        const serviceCollection=client.db('travelGuide').collection('services');
-        const reviewCollection=client.db('travelGuide').collection('review');
 
-        app.get('/services', async(req, res)=>{
-            const query={}
-            const cursor=serviceCollection.find(query);
-            const services=await cursor.limit(3).toArray();
+// jwt
+// function verifyJwt(req, res, next) {
+//     // console.log(req.headers.authorization)
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorization' })
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_CECRET, function (err, decoded) {
+//         if (err) {
+//             return res.status(401).send({ message: 'unauthorization access' })
+//         }
+//         req.decoded = decoded;
+//         next()
+//     })
+
+// }
+
+async function run() {
+    try {
+        const serviceCollection = client.db('travelGuide').collection('services');
+        const reviewCollection = client.db('travelGuide').collection('review');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
             res.send(services);
         })
-        app.get('/allservices', async(req, res)=>{
-            const query={}
-            const cursor=serviceCollection.find(query);
-            const services=await cursor.toArray();
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
             res.send(services);
         })
 
-        app.get('/services/:id', async(req, res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const details=await serviceCollection.findOne(query);
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const details = await serviceCollection.findOne(query);
             res.send(details);
         })
         // add service
 
-        app.post('/addServices', async(req, res)=>{
-            const addService=req.body;
-            const result=await serviceCollection.insertOne(addService);
+        app.post('/addServices', async (req, res) => {
+            const addService = req.body;
+            const result = await serviceCollection.insertOne(addService);
             res.send(result);
         })
         // Review
-        app.post('/review', async(req, res)=>{
-            const review=req.body;
-            const result=await reviewCollection.insertOne(review);
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
 
-        app.get('/review', async(req, res)=>{
-            const query={};
-           
-            const cursor= reviewCollection.find(query);
-            const review=await cursor.toArray();
+        app.get('/review', async (req, res) => {
+
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
             res.send(review);
         })
-        app.delete('/review/:id', async(req, res)=>{
-            const id=req.params.id;
+
+
+// delete data
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
             console.log(id);
             const query = { _id: ObjectId(id) };
-            const result=await reviewCollection.deleteOne(query);
+            const result = await reviewCollection.deleteOne(query);
             res.send(result);
-            
+
         })
-       
+        // // jwt
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body;
+        //     // console.log(user);
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_CECRET)
+        //     res.send({ token });
+        // })
+
     }
-    finally{
+    finally {
 
     }
 
 }
-run().catch(error=> console.error(error))
+run().catch(error => console.error(error))
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Travel Guide in on going....')
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Travel Guide server is done ${port}`);
 })
